e390ae913883b1b455d96d3b040d446edcd7e509
mkenchappajunjanna1 <mkenchappajunjanna1@sheffield.ac.uk>
2024-02-15 21:27:51 +0000

    added support for daisy UI

--- client/src/pages/landing/LandingPage.tsx
+++ client/src/pages/landing/LandingPage.tsx
@@ -3,14 +3,20 @@ import introWaste from "../../assets/intro_waste.svg";
 
 const AboutPage: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={introWaste} className="h-80 w-80 m-4" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <div className="text-3xl font-bold underline">Hello Team Titans</div>
-      </header>
+    <div className="diff aspect-[16/9]">
+      <div className="diff-item-1">
+        <div className="bg-primary text-primary-content text-5xl font-black grid place-content-center">
+          <img src={introWaste} className="w-52 h-52 mb-5" />
+          TITANS
+        </div>
+      </div>
+      <div className="diff-item-2">
+        <div className="bg-base-200 text-5xl font-black grid place-content-center">
+          <img src={introWaste} className="w-52 h-52 mb-5" />
+          TITANS
+        </div>
+      </div>
+      <div className="diff-resizer"></div>
     </div>
   );
 };
