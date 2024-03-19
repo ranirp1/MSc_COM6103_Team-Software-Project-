e6f10ca0058a529cc3623dca21c0797d4536b3d9
Subhashini Vijayraj <svijayraj1@sheffield.ac.uk>
2024-03-19 02:54:46 +0000

    Ui Updates

--- client/src/pages/User/DeviceListPage.tsx
+++ client/src/pages/User/DeviceListPage.tsx
@@ -397,13 +397,28 @@ const UserDashboard = () => {
               onChange={handleSearchChange}
             />
           </form>
+          <details className="dropdown dropdown-end ml-4">
+            <summary tabIndex={0} className="btn btn-ghost cursor-pointer">
+              <RiFilter3Line className="text-lg" /> Filter
+            </summary>
+            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+              <li>
+                <a onClick={() => handleFilterChange("ascending")}>Ascending</a>
+              </li>
+              <li>
+                <a onClick={() => handleFilterChange("descending")}>
+                  Descending
+                </a>
+              </li>
+            </ul>
+          </details>
           <div className="dropdown dropdown-end ml-4">
-            <label tabIndex={0} className="btn btn-ghost cursor-pointer">
+            <label tabIndex={0} role="button" className="btn btn-ghost cursor-pointer">
               <RiFilter3Line className="text-lg" /> Filter
             </label>
             <ul
               tabIndex={0}
-              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+              className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
                 <a onClick={() => handleFilterChange("ascending")}>Ascending</a>
