28e869815c06b989b1bb374bf2bc9121d9d8bf9c
Subhashini Vijayraj <svijayraj1@sheffield.ac.uk>
2024-04-23 22:57:05 +0100

    Update database credentials in app.py

--- server/app.py
+++ server/app.py
@@ -20,7 +20,7 @@ from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost:3306/test_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:12345@localhost:3306/test_db'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:your_password@127.0.0.0:3306/test_db'
 db = SQLAlchemy(app)
 CORS(app)
