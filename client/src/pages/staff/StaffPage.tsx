5dfadca6a45cacfd2eb6cdccdd1a0afeb8a5f3d8
rani kumari <rkumari1@sheffield.ac.uk>
2024-04-29 17:45:43 +0100

    updated

--- client/src/pages/staff/StaffPage.tsx
+++ client/src/pages/staff/StaffPage.tsx
@@ -24,6 +24,7 @@ import { IoSend } from "react-icons/io5";
 import { DeviceClassification } from "../landing/CheckNow";
 import DeviceClassificationComponent from "./DeviceClassificationComponent";
 import ReportDialog from "./component/ReportDialog";
+
 class Device {
   id: number;
   brand: string;
@@ -45,6 +46,7 @@ class Device {
   dataRetrievalLink?: string;
   user_name?: string;
   user_phone?: string;
+  userDeviceID?: number;
 
   constructor(
     id: number,
@@ -66,7 +68,8 @@ class Device {
     user_email?: string,
     dataRetrievalLink?: string,
     user_name?: string,
-    user_phone?: string
+    user_phone?: string,
+    userDeviceId?: number
   ) {
     this.id = id;
     this.brand = manufacturer;
@@ -88,6 +91,7 @@ class Device {
     this.dataRetrievalLink = dataRetrievalLink;
     this.user_name = user_name;
     this.user_phone = user_phone;
+    this.userDeviceID = userDeviceId;
   }
 
   static fromJson(json: any): Device {
@@ -111,7 +115,8 @@ class Device {
       json.user_email,
       json.dataRetrievalLink,
       json.user_name,
-      json.user_phone
+      json.user_phone,
+      json.userDeviceId
     );
   }
 }
@@ -411,6 +416,7 @@ const StaffDashboard = () => {
       const data = {
         email: localDevice.user_email,
         urlLink: localDevice.dataRetrievalLink,
+        userDeviceId: localDevice.userDeviceID,
       };
 
       try {
@@ -581,11 +587,11 @@ const StaffDashboard = () => {
                     }
                     className="mt-1 block w-full select select-bordered bg-gray-200 text-black"
                   >
-                    <option value="DEV_REGISTERED">Device Registered</option>
-                    <option value="DEV_VERIF">Device Verified</option>
-                    <option value="PAYMENT_DONE">Payment Processed</option>
-                    <option value="DATA_RETRIEVED">Data Retrieved</option>
-                    <option value="URL_READY">Link Received</option>
+                    <option value="Device Registered">Device Registered</option>
+                    <option value="Device Verified">Device Verified</option>
+                    <option value="Payment Processed">Payment Processed</option>
+                    <option value="Data Retrieved">Data Retrieved</option>
+                    <option value="Link Received">Link Received</option>
                   </select>
                 </div>
                 <div>
@@ -666,13 +672,13 @@ const StaffDashboard = () => {
         {renderCexLink()}
         {editMode && (
           <button
-            className="btn btn-primary  w-full mb-10"
+            className="btn btn-primary  w-full "
             onClick={saveDeviceUpdates}
           >
             Save Changes
           </button>
         )}
-
+        <div className=" h-4"></div>
         <div className="text-bold text-primary text-lg font-bold pt-5 border-t-2">
           User Contact Information
         </div>
@@ -766,10 +772,10 @@ const StaffDashboard = () => {
                           Created At
                         </th>
                         <th className="text-black text-lg font-bold min-w-[250px]">
-                          Classification
+                          Status
                         </th>
                         <th className="text-black text-lg font-bold min-w-[250px]">
-                          Status
+                          Classification
                         </th>
                       </tr>
                       </thead>
@@ -795,7 +801,7 @@ const StaffDashboard = () => {
                                               ""
                                           )}
                                           alt={device.image}
-                                          className="h-24 w-16 fel flex-col place-content-center place-items-center items-center bg-primary bg-opacity-90 rounded shadow"/>
+                                          className="object-contain h-24 w-16 fel flex-col place-content-center place-items-center items-center bg-white  rounded shadow"/>
                                   ) : (
                                       <div
                                           className="h-24 w-16 felx flex-col place-content-center place-items-center items-center bg-primary bg-opacity-90 rounded shadow">
@@ -809,14 +815,15 @@ const StaffDashboard = () => {
                                 <td className="text-lg">{device.brand}</td>
                                 <td className="text-lg">{device.model}</td>
                                 <td className="text-lg">{device.createdAt}</td>
-                                <td className="text-lg">
-                                  {device.classification || "Not Classified"}
-                                </td>
                                 <td>
                                   <div className="flex">
                                     <DeviceStatusBadge
                                         status={device.device_status ?? ""}/>
                                   </div>
+                                </td>
+                                <td>
+                                  <DeviceClassificationComponent
+                                      deviceClassification={device.classification || "Not Classified"}/>
                                 </td>
                               </tr>
                           ))
@@ -846,7 +853,7 @@ const StaffDashboard = () => {
                         Yes
                       </button>
                       <button
-                          className="btn btn-ghost w-1/2"
+                          className="btn btn-ghost w-1/2  border-primary border-2"
                           onClick={() => setShowLogoutModal(false)} // Close modal on 'No'
                       >
                         No
@@ -860,11 +867,13 @@ const StaffDashboard = () => {
           <div className="fixed bottom-4 right-4 flex flex-col ">
             {isAdminAndStaff ? (
                 <button
-                    className="btn  shadow-2xl btn-ghost text-primary h-20 mb-3 rounded-full"
+                    className="btn  shadow-2xl btn-ghost text-primary h-20 mb-3 rounded-full border-primary border-2"
                     onClick={() => (window.location.href = "/admin?isAdminAndStaff=true")}
                 >
                   <AiOutlineUserSwitch size={40}/>
-                  <div className="pl-2 text-lg ">Switch to Admin</div>
+                  <div className="pl-2 text-lg  ">
+                    Switch to Admin
+                  </div>
                 </button>
             ) : null}
             <button className="btn    shadow-2xl bg-primary text-white h-20 rounded-full">
@@ -873,7 +882,6 @@ const StaffDashboard = () => {
             </button>
           </div>
         </div>
-
         <button
             className="btn fixed bottom-4 right-4 shadow-2xl bg-primary text-white h-20 rounded-full"
             onClick={() => {
@@ -891,7 +899,7 @@ const StaffDashboard = () => {
         <dialog id="my_modal_1" className="modal">
           <ReportDialog/>
         </dialog>
-        </>
+      </>
   );
 };
 export default StaffDashboard;
